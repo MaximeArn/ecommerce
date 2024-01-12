f2a0a0172f2ae6d916bc49df1270e511f9a8b5e3
MaximeArn <maxime-arnould@outlook.fr>
2024-01-12 15:29:55 +0100

    on sellerPage request get the items that belongs to the user and send it to the view

--- controllers/sellers.js
+++ controllers/sellers.js
@@ -1,7 +1,10 @@
+const Item = require("../models/item");
+
 module.exports = {
   renderSellerPage: async (req, res, next) => {
     try {
-      res.render("sellers/sellerPage.ejs");
+      const sellerItems = await Item.find({ seller: req.user.username });
+      res.render("sellers/sellerPage.ejs", { item: sellerItems });
     } catch (error) {
       next(error);
     }
--- middlewares/sellers.js
+++ middlewares/sellers.js
@@ -2,9 +2,6 @@ const AuthenticationError = require("../errors/authError");
 
 module.exports = async ({ user: { role } }, res, next) => {
   try {
-    console.log("seller midd");
-    console.log(role);
-    console.log(role !== "seller");
     if (role !== "seller")
       throw new AuthenticationError(
         "You do not have the necessary permissions to access this resource. This action is reserved for sellers. ",

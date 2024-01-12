a1112ed6316c68d5a9d5e44d94d38568a0d12baf
MaximeArn <maxime-arnould@outlook.fr>
2024-01-12 15:53:22 +0100

    map on sellerItems to fill the sellerPage and style it

--- controllers/sellers.js
+++ controllers/sellers.js
@@ -4,7 +4,7 @@ module.exports = {
   renderSellerPage: async (req, res, next) => {
     try {
       const sellerItems = await Item.find({ seller: req.user.username });
-      res.render("sellers/sellerPage.ejs", { item: sellerItems });
+      res.render("sellers/sellerPage.ejs", { items: sellerItems });
     } catch (error) {
       next(error);
     }

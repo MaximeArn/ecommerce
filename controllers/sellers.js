821fe796892e215a766d419bdb03915892e27d72
MaximeArn <maxime-arnould@outlook.fr>
2024-01-17 17:17:21 +0100

    createItem method implemented

--- controllers/sellers.js
+++ controllers/sellers.js
@@ -17,6 +17,13 @@ module.exports = {
       next(error);
     }
   },
+  renderCreateItem: async (req, res, next) => {
+    try {
+      res.render("sellers/addItem.ejs");
+    } catch (error) {
+      next(error);
+    }
+  },
   updateItem: async ({ params: { id }, body }, res, next) => {
     try {
       const item = await Item.findOne({ _id: id });
@@ -44,6 +51,20 @@ module.exports = {
     try {
       await Item.deleteOne({ _id: id });
       res.status(200).send("Item has been deleted");
+    } catch (error) {
+      next(error);
+    }
+  },
+  createItem: async ({ body, user }, res, next) => {
+    try {
+      if (body.tags.length < 3)
+        throw new Error("you need at least 3 tags to describe your item.");
+      Object.entries(body).map(([key, value]) => {
+        if (typeof value === "string" && value.trim() === "")
+          throw new Error(`${key} field is required.`);
+      });
+      await new Item({ ...body, seller: user.username }).save();
+      res.end();
     } catch (error) {
       next(error);
     }
--- routers/sellers.js
+++ routers/sellers.js
@@ -4,12 +4,16 @@ const {
   renderUpdateItemPage,
   updateItem,
   deleteItem,
+  renderCreateItem,
+  createItem,
 } = require("../controllers/sellers");
 
 const sellersRouter = Router();
 
 sellersRouter.get("/", renderSellerPage);
 sellersRouter.get("/item/:itemId", renderUpdateItemPage);
+sellersRouter.get("/addItem", renderCreateItem);
+sellersRouter.post("/addItem", createItem);
 sellersRouter.patch("/item/:id", updateItem);
 sellersRouter.delete("/item/:id", deleteItem);
 
